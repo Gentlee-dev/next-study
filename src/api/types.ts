16e6fc28886945053a74aa3bd463aba20c4844ec
leegentle <gentlee.dev@gmail.com>
2025-02-16 23:57:54 +0900

    getStaticProps with post

---
+++ src/api/types.ts
@@ -0,0 +1,3 @@
+export interface ErrorRespones {
+  error_message: string;
+}
---
+++ src/pages/post/index.tsx
@@ -0,0 +1,21 @@
+import { fetchPost, Post } from "@/api/post";
+import { PostMain } from "@/domains/post";
+import dayjs from "dayjs";
+import { GetStaticProps } from "next";
+import { ComponentProps, FC } from "react";
+
+type Props = ComponentProps<typeof PostMain>;
+
+export const getStaticProps: GetStaticProps<Props> = async () => {
+  const post = await fetchPost("2");
+
+  return {
+    props: { post, date: dayjs().format("YYYY-mm-dd HH:mm:ss") },
+  };
+};
+
+const PostMainPage: FC<Props> = (props) => {
+  return <PostMain {...props} />;
+};
+
+export default PostMainPage;
